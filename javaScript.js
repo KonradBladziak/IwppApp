8366cdaff26505772723e267ac4e310d4134f88f
MichalCzajkowski123 <103947663+MichalCzajkowski1806@users.noreply.github.com>
2024-04-26 14:35:07 +0200

    Działające obliczenia
    Dopasowanie id z formularza, dopisanie wyświetlania wyników w dokumencie

--- javaScript.js
+++ javaScript.js
@@ -3,44 +3,51 @@ function PrzekrojWlewka()
 {
     var Pp;
 
-    if (document.getElementById("kwadrat/prostokat").selected)
+    if (document.getElementById("kwadratowy").checked)
     {
-        var a = document.getElementById("bokAWlekwa");
-        var b = document.getElementById("bokBWlekwa");
+        var a = document.getElementById("bokA").value;
+        var b = document.getElementById("bokB").value;
         Pp = a * b;
     }
-    else if (document.getElementById("kolo").selected)
+    else if (document.getElementById("okragly").checked)
     {
-        var r = document.getElementById("promienWlekwa");
+        var r = document.getElementById("promien").value;
 
         Pp = Math.PI * Math.pow(r, 2);
     }
-
     return Pp;
 }
 
 function WydajnoscMasowa()
 {
-    var a = document.getElementById("iloscZylCOS");
-    var Vodl = document.getElementById("LiniowaVOdlewania");
-    var Ps = document.getElementById("GestoscStaliStaly");
+    var a = document.getElementById("zyly").value;
+    var Vodl = document.getElementById("liniowaPO").value;
+    var Ps = document.getElementById("gestoscSWSS").value;
     var Qm = a * PrzekrojWlewka() * Vodl * Ps;
-
+    var wynikWydajnosc = document.getElementById("przeplywMasResult");
+    wynikWydajnosc.innerHTML = Qm.toFixed(1);
     return Qm;
 }
 
 function PrzeliczanieStrumieniaMasowego()
 {
-    var Pc = document.getElementById("GestoscStaliCiekly");
+    var Pc = document.getElementById("gestoscSWSC").value;
     var Qv = WydajnoscMasowa() / Pc;
-
+    var wynikPrzeliczenia = document.getElementById("przeplywObjResult");
+    wynikPrzeliczenia.innerHTML = Qv.toFixed(1);
     return Qv;
 }
 
 function NatezeniePrzeplywuCieczy()
 {
-    var SL = document.getElementById("SkalaSL");
+    var SL = document.getElementById("skalaSi").value;
     var Qvprim = Math.sqrt(Math.pow(SL, 5)) * PrzeliczanieStrumieniaMasowego();
+    var wynikPrzeplywu = document.getElementById("przeplywObjCieczyMS");
+    wynikPrzeplywu.innerHTML = Qvprim.toFixed(1);
+
+    var zamianaJednostek = Qvprim * (1000 * 60);
+    var wynikPrzeplywuZamianaJednostek = document.getElementById("przeplywObjCieczyDmMin");
+    wynikPrzeplywuZamianaJednostek.innerHTML = zamianaJednostek.toFixed(1);
 
     return Qvprim;
 }
